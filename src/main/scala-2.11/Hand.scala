c12d48f0b436f7f3172e4449987e2534871fabf0
Guitar1st <diman4eg@mail.ru>
2015-06-10 23:39:41 +0300

    continue writing hand comparer

--- src/main/scala-2.11/Hand.scala
+++ src/main/scala-2.11/Hand.scala
@@ -5,6 +5,17 @@
 object Hand {
   val suits = Seq('h', 'd', 'c', 's')
   val values = Seq('2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A')
+  val combinationOrder = Seq(
+    "HIGH_CARD",
+    "ONE_PAIR",
+    "TWO_PAIR",
+    "SET",
+    "STRAIGHT",
+    "FLUSH",
+    "FULL_HOUSE",
+    "FOUR",
+    "STRAIGHT_FLUSH"
+  )
   def possibleCards = {
     for {
         i1 <- values
@@ -14,12 +25,12 @@ object Hand {
 }
 
 class Hand(val cards: String = "") {
+  val handSuits = for (i <- cards if cards.indexOf(i) % 2 == 1) yield i
+  val handVals = for (i <- cards if cards.indexOf(i) % 2 == 0) yield i
   isCorrect
   def hasCorrectLen = cards.length == 10
   def isCorrect = {
     if (hasCorrectLen) {
-      val handSuits = for (i <- cards if cards.indexOf(i) % 2 == 1) yield i
-      val handVals = for (i <- cards if cards.indexOf(i) % 2 == 0) yield i
       val correctSuits = handSuits.foldLeft(true)((a, b) => a & (Hand.suits contains b))
       val correctVals = handVals.foldLeft(true)((a, b) => a & (Hand.values contains b))
       if (!correctSuits) {
@@ -28,6 +39,12 @@ class Hand(val cards: String = "") {
         throw new IllegalArgumentException("hand value is wrong!")
       } else true
     } else throw new IllegalArgumentException("hand length is wrong!")
+  }
+  def combination = {
+    val diffSuits = handSuits.toSet
+    //val diffVals = handVals.toSet
+    println("Different suits:    " +  diffSuits.size.toString)
+    if (diffSuits.size == 1) "FLUSH" else "SOMETHING_ELSE"
   }
   def isGreaterThan(otherHand: Hand) {
     println("this hand: " + this.cards)
--- src/test/scala-2.11/HandTest.scala
+++ src/test/scala-2.11/HandTest.scala
@@ -20,7 +20,14 @@ class HandTest extends FunSuite {
       new Hand("2d3d4d5p6h")
     }
     assert(thrown.getMessage === "hand suit is wrong!")
+    new Hand("2d3dTd5d6d")
+  }
 
-    new Hand("2d3dTd5d6h")
+  test("method combination() works correctly") {
+    val flushHand = new Hand("2d3dTd5d6d")
+    assert(flushHand.combination === "FLUSH")
+
+    val notFlushHand = new Hand("2d3dTs5d6d")
+    assert(notFlushHand.combination === "SOMETHING_ELSE")
   }
 }
