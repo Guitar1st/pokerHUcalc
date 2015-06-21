8abee8c03feeb980f9771d490649a9a7167238dc
Guitar1st <diman4eg@mail.ru>
2015-06-21 14:12:23 +0300

    continue to write hand comparer

--- src/main/scala-2.11/Hand.scala
+++ src/main/scala-2.11/Hand.scala
@@ -36,38 +36,65 @@ class Card(val value: CardValue, val suit: Suit) extends Ordered[Card] {
   def completelySame(that: Card): Boolean = sameSuit(that) & sameNumber(that)
 }
 
-abstract class Hand(val order: Int = 0) extends Ordered[Hand] {
-  def orderCounted = order
-  def compare(that: Hand): Int = this.orderCounted - that.orderCounted
-}
-
-class PlayersHand(c1: Card, c2: Card, c3: Card, c4: Card, c5: Card) extends Hand {
+class PlayersHand(c1: Card, c2: Card, c3: Card, c4: Card, c5: Card) {
   val cards = List(c1, c2, c3, c4, c5)
   val cardsHR = for (c <- cards) yield (c.value, c.suit)
+  val cardsNumbersSorted = (for (c <- cards) yield c.value.number).sorted
   if (checkSameCards) throw new IllegalArgumentException("hand has same cards")
+  println(cardsHR)
   def checkSameCards = {
     val checkList = for {
-      card_one <- cards
-      card_two <- cards
+        card_one <- cards
+        card_two <- cards
       if card_one != card_two
     } yield card_one completelySame card_two
     checkList.foldLeft(false)(_|_)
   }
-  println(checkSameCards)
-  println(cardsHR)
-  println(orderCounted)
-  override def orderCounted = 0 // Todo: write this correctly
+  def isFlush = {
+    val suitList = for (c <- cards) yield c.suit
+    suitList.toSet.size == 1
+  }
+  def isStraight = {
+    val lower = cardsNumbersSorted.head
+    val straightFromHead = for (j <- 0.to(4)) yield lower + j
+    val straightFromAce = List(2, 3, 4, 5, 14)
+    cardsNumbersSorted match {
+      case `straightFromHead` => lower
+      case `straightFromAce` => 1
+      case _ => 0
+    }
+  }
+  def force = {
+    // todo: finish this func
+    lazy val straightVal = isStraight
+    if (isFlush) {
+      if (straightVal != 0) {new HandForce(Hand.StraightFlush, straightVal)} else Hand.Flush
+    } else if (straightVal != 0) {
+      new HandForce(Hand.Straight, straightVal)
+    } else new HandForce(Hand.HighCard, 0)
+  }
 }
+
+
+abstract class Hand(val order: Int = 0)
 
 object Hand {
-  case object StraightFlush extends Hand(9)
-  case object Four extends Hand(8)
-  case object FullHouse extends Hand(7)
-  case object Flush extends Hand(6)
-  case object Straight extends Hand(5)
-  case object Set extends Hand(4)
-  case object TwoPair extends Hand(3)
-  case object OnePair extends Hand(2)
-  case object HighCard extends Hand(1)
+  case object StraightFlush extends Hand(900)
+  case object Four extends Hand(800)
+  case object FullHouse extends Hand(700)
+  case object Flush extends Hand(600)
+  case object Straight extends Hand(500)
+  case object Set extends Hand(400)
+  case object TwoPair extends Hand(300)
+  case object OnePair extends Hand(200)
+  case object HighCard extends Hand(100)
 }
 
+class HandForce(val handType: Hand, val internalForce: Int = 0) extends Ordered[HandForce] {
+  val force = handType.order + internalForce
+  def compare(that: HandForce): Int = this.force - that.force
+  override def equals(that: Any): Boolean = that match {
+    case that: HandForce => this.force == that.force
+    case _ => false
+  }
+}
--- src/test/scala-2.11/HandTest.scala
+++ src/test/scala-2.11/HandTest.scala
@@ -27,4 +27,35 @@ class HandTest extends FunSuite {
     assert((card3 completelySame card4) === false)
     assert((card5 completelySame card6) === true)
   }
+
+  test("hand order methods work correctly") {
+    val card1 = new Card(CardValue.Two, Suit.Hearts)
+    val card2 = new Card(CardValue.Three, Suit.Hearts)
+    val card3 = new Card(CardValue.Four, Suit.Hearts)
+    val card4 = new Card(CardValue.Five, Suit.Hearts)
+    val card5 = new Card(CardValue.Six, Suit.Hearts)
+    val card6 = new Card(CardValue.Seven, Suit.Hearts)
+    val card7 = new Card(CardValue.Eight, Suit.Hearts)
+    val card8 = new Card(CardValue.Nine, Suit.Hearts)
+    val card9 = new Card(CardValue.Ten, Suit.Hearts)
+    val card10 = new Card(CardValue.Jack, Suit.Hearts)
+    val card11 = new Card(CardValue.Queen, Suit.Hearts)
+    val card12 = new Card(CardValue.King, Suit.Hearts)
+    val card13 = new Card(CardValue.Ace, Suit.Hearts)
+    val card14 = new Card(CardValue.Ace, Suit.Spades)
+
+    val hand1 = new PlayersHand(card1, card2, card3, card4, card5)
+    assert(hand1.isFlush === true)
+    assert(hand1.isStraight === 2)
+    assert(hand1.force === new HandForce(Hand.StraightFlush, 2))
+    val hand2 = new PlayersHand(card1, card2, card3, card4, card14)
+    assert(hand2.isFlush === false)
+    assert(hand2.isStraight === 1)
+    assert(hand2.force === new HandForce(Hand.Straight, 1))
+    val hand3 = new PlayersHand(card1, card2, card3, card4, card6)
+    assert(hand3.isStraight === 0)
+    val hand4 = new PlayersHand(card9, card10, card11, card12, card13)
+    assert(hand4.isStraight === 10)
+    assert(hand4.force === new HandForce(Hand.StraightFlush, 10))
+  }
 }
